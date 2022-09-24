a63302745d1bbd4f7dc5d1f804cf32c253b71096
unknown <sidhodhank2018@gmail.com>
2022-09-24 12:53:53 +0530

    Add Last Name Validation Using Regex

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -5,25 +5,33 @@ import java.util.regex.*;
 
 public class UserRegistrationProblem {
 	// UserName method for check Name Validation
-	public static void UserName(String input) {
+	public static void UserFirstLastName(String input) {
 		Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}");
 		Matcher matcher = pattern.matcher(input);
 		boolean matchFound = matcher.find();
 		if (matchFound)
-			System.out.println("Valid Name");
+			System.out.println("Valid Name\n");
 		else
-			System.out.println("Invalid Name");
+			System.out.println("Invalid Name\n");
 	}
 
 	// main
 	public static void main(String[] args) {
 		System.out.println("***************User Registration Problem*******************");
 
-		// Get User name
-		Scanner sc = new Scanner(System.in);
+		// Get User First Name
+		Scanner sc1 = new Scanner(System.in);
 		System.out.print("Enter the First Name:");
-		String FirstName = sc.next();
-		UserName(FirstName);
+		String FirstName = sc1.next();
+		// pass first name to method
+		UserFirstLastName(FirstName);
+
+		// Get User Last Name
+		Scanner sc2 = new Scanner(System.in);
+		System.out.print("Enter the Last Name:");
+		String LastName = sc2.next();
+		// Pass last name
+		UserFirstLastName(LastName);
 
 	}
 
