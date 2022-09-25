77e5a7fa57efd30012b9ff3ae6b82881504d9aa6
unknown <sidhodhank2018@gmail.com>
2022-09-25 18:43:38 +0530

    Add JUnit Test for Multiple Email Address

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -10,49 +10,15 @@ public class UserRegistrationProblem {
 		this.input = string;
 	}
 
-	// UserName method for check Name Validation
-	public static String UserFirstLastName() {
-		Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}");
-		Matcher matcher = pattern.matcher(input);
-		boolean matchFound = matcher.find();
-		if (matchFound)
-			return "Valid Name";
-		else
-			return "Invalid Name";
-	}
-
 	// Email method for check Email Validation
 	public static String EmailAddress() {
-		Pattern pattern = Pattern.compile("^(abc.)[a-z]+@(bl.co.)[a-z]{2}$");
+		Pattern pattern = Pattern.compile("^(.+)@(.+)$");
 		Matcher matcher = pattern.matcher(input);
 		boolean matchFound = matcher.find();
 		if (matchFound)
 			return "Valid Email Address";
 		else
 			return "Invalid Email Address";
-	}
-
-	// Mobile Number method for check Mobile Number Validation
-	public static String MobileNumber() {
-		Pattern pattern = Pattern.compile("^([+]\\d{2})?\\d{10}$");
-		Matcher matcher = pattern.matcher(input);
-		boolean matchFound = matcher.find();
-		if (matchFound)
-			return "Valid Mobile Number";
-		else
-			return "Invalid Mobile Number";
-	}
-
-	// Password method for check Password Validation
-	public static String Password() {
-		//
-		Pattern pattern = Pattern.compile("(?=.*?[0-9])(?=.*[A-Z])(?=.*?[#?!@$%^&*-]).{8,}");
-		Matcher matcher = pattern.matcher(input);
-		boolean matchFound = matcher.find();
-		if (matchFound)
-			return "Valid Password";
-		else
-			return "Invalid Password";
 	}
 
 }
--- UserRegistrationProblemTest.java
+++ UserRegistrationProblemTest.java
@@ -6,48 +6,54 @@ import org.junit.Test;
 import stack.ExceptionDemo;
 
 public class UserRegistrationProblemTest {
+	// JUnit Test for sample emails
 	@Test
-	public void CheckFirstNameValidation() {
-		UserRegistrationProblem FirstName = new UserRegistrationProblem("Sai");
-		String actualResult = FirstName.UserFirstLastName();
-		
-		Assert.assertEquals("Valid Name",actualResult);
-		
-	}
-	@Test
-	public void CheckLastNameValidation() {
-		UserRegistrationProblem LastName = new UserRegistrationProblem("Kamble");
-		String actualResult = LastName.UserFirstLastName();
-		
-		Assert.assertEquals("Valid Name",actualResult);
-		
-	}
-	@Test
-	public void CheckEmailValidation() {
-		UserRegistrationProblem Email = new UserRegistrationProblem("abc.xyz@bl.co.in");
+	public void CheckEmailValidation1() {
+		UserRegistrationProblem Email = new UserRegistrationProblem("abc@yahoo.com");
 		String actualResult = Email.EmailAddress();
-		
-		Assert.assertEquals("Valid Email Address",actualResult);
-		
+		Assert.assertEquals("Valid Email Address", actualResult);
 	}
-	@Test
-	public void CheckMobileNumberValidation() {
-		UserRegistrationProblem MobileNumber = new UserRegistrationProblem("+910123456789");
-		String actualResult = MobileNumber.MobileNumber();
-		
-		Assert.assertEquals("Valid Mobile Number",actualResult);
-		
-	}
-	@Test
-	public void CheckPasswordValidation() {
-		UserRegistrationProblem Password = new UserRegistrationProblem("Pass@1asdf");
-		String actualResult = Password.Password();
-		
-		Assert.assertEquals("Valid Password",actualResult);
-		
-	}
-	
 
-	
+	@Test
+	public void CheckEmailValidation2() {
+		UserRegistrationProblem Email = new UserRegistrationProblem("abc-100@yahoo.com");
+		String actualResult = Email.EmailAddress();
+		Assert.assertEquals("Valid Email Address", actualResult);
+	}
+
+	@Test
+	public void CheckEmailValidation3() {
+		UserRegistrationProblem Email = new UserRegistrationProblem("abc123@.com.my");
+		String actualResult = Email.EmailAddress();
+		Assert.assertEquals("Valid Email Address", actualResult);
+	}
+
+	@Test
+	public void CheckEmailValidation4() {
+		UserRegistrationProblem Email = new UserRegistrationProblem("abc.100@yahoo.com");
+		String actualResult = Email.EmailAddress();
+		Assert.assertEquals("Valid Email Address", actualResult);
+	}
+
+	@Test
+	public void CheckEmailValidation5() {
+		UserRegistrationProblem Email = new UserRegistrationProblem("abc111@abc.com");
+		String actualResult = Email.EmailAddress();
+		Assert.assertEquals("Valid Email Address", actualResult);
+	}
+
+	@Test
+	public void CheckEmailValidation6() {
+		UserRegistrationProblem Email = new UserRegistrationProblem("abc+100@gmail.com");
+		String actualResult = Email.EmailAddress();
+		Assert.assertEquals("Valid Email Address", actualResult);
+	}
+
+	@Test
+	public void CheckEmailValidation7() {
+		UserRegistrationProblem Email = new UserRegistrationProblem("abc@gmail.com.com");
+		String actualResult = Email.EmailAddress();
+		Assert.assertEquals("Valid Email Address", actualResult);
+	}
 
 }
