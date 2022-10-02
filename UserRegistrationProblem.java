0b4dd720f1a2253e3251bbba4d4af89ffdaa0b3b
unknown <sidhodhank2018@gmail.com>
2022-10-02 21:04:54 +0530

    Modify Patter Of Email Address

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -1,38 +1,32 @@
 package regex;
 
-import java.util.Scanner;
 import java.util.regex.*;
 
 public class UserRegistrationProblem {
-	
 
 	// Email method for check Email Validation
 	public static void EmailAddress(String input) {
-		Pattern pattern = Pattern.compile("[a-z0-9]+@[a-z]+\\.[a-z]{2,3}");
+		Pattern pattern = Pattern.compile("^(abc)[-.+0-9a-z]*@[a-z0-9]+[.][a-z]{0,3}[.]?[a-z]{0,2}$");
 		Matcher matcher = pattern.matcher(input);
 		boolean matchFound = matcher.find();
 		if (matchFound)
-			System.out.println(input+" :-Valid Email Address\n");
+			System.out.println(input + " :-Valid Email Address\n");
 		else
-			System.out.println(input+" :-Invalid Email Address\n");
+			System.out.println(input + " :-Invalid Email Address\n");
 	}
-
-	
 
 	// main
 	public static void main(String[] args) {
 		System.out.println("***************User Registration Problem*******************");
-        //Samples of emails
-		String[] emails = {"abc@yahoo.com","abc-100@yahoo.com","abc123@.com.my","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com",
-				"abc@gmail.com.com","abc+100@gmail.com"};
-				
+		// Samples of emails
+		String[] emails = { "abc@yahoo.com", "abc-100@yahoo.com", "abc123@.com.my", "abc.100@yahoo.com",
+				"abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com",
+				"abc+100@gmail.com" };
 
 		// pass emails to method email
-		for(int i=0; i<=emails.length-1; i++) {
-		EmailAddress(emails[i]);
+		for (int i = 0; i <= emails.length - 1; i++) {
+			EmailAddress(emails[i]);
 		}
-
-	
 
 	}
 
